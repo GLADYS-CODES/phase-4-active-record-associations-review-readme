5287e7cb821bf014221f2e4f8a7a90d9e51fc2cd
GLADYS <mungaiglado106@gmail.com>
2022-09-27 14:21:09 +0300

    inital commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ApplicationRecord
+    #one to many
+    has_many :posts
+
+    #one to one
+    #add this:
+    has_one :profile
+end
+
+class Profile < ApplicationRecord
+
+    belongs_to :author
 end
